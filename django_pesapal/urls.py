9f5d8bc65071684023ac04b308c2e1732d904597
odero <billyx5@gmail.com>
2014-06-30 15:03:39 +0300

    Fix import bug in urls.py

--- django_pesapal/urls.py
+++ django_pesapal/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import patterns, url
 from django.contrib.auth.decorators import login_required
 
-from .views import CustomTransactionCompletedView
+from .views import TransactionCompletedView
 
 urlpatterns = patterns(
     '',
-    url(r'^transaction/completed/$', login_required(CustomTransactionCompletedView.as_view()), name='transaction_completed'),
+    url(r'^transaction/completed/$', login_required(TransactionCompletedView.as_view()), name='transaction_completed'),
 )
