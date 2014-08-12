4caf9cf1393984e5882d14773aa2cc80b17f965c
odero <billyx5@gmail.com>
2014-08-12 17:38:36 +0300

    Bump version to 0.1.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-pesapal',
-    version='0.1.1',
+    version='0.1.2',
     packages=[
         'django_pesapal'
     ],
