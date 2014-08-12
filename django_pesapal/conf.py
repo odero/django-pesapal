deb6acf0f4d6aec9496c0c6f15b1a3ca2c6cca47
odero <billyx5@gmail.com>
2014-08-12 17:37:33 +0300

    Live URL uses https

--- django_pesapal/conf.py
+++ django_pesapal/conf.py
@@ -8,7 +8,7 @@ PESAPAL_DEMO = getattr(settings, 'PESAPAL_DEMO', True)
 if PESAPAL_DEMO:
     PESAPAL_IFRAME_LINK = getattr(settings, 'PESAPAL_IFRAME_LINK', 'http://demo.pesapal.com/api/PostPesapalDirectOrderV4')
 else:
-    PESAPAL_IFRAME_LINK = getattr(settings, 'PESAPAL_IFRAME_LINK', 'http://pesapal.com/api/PostPesapalDirectOrderV4')
+    PESAPAL_IFRAME_LINK = getattr(settings, 'PESAPAL_IFRAME_LINK', 'https://www.pesapal.com/api/PostPesapalDirectOrderV4')
 
 PESAPAL_CONSUMER_KEY = getattr(settings, 'PESAPAL_CONSUMER_KEY', '')
 PESAPAL_CONSUMER_SECRET = getattr(settings, 'PESAPAL_CONSUMER_SECRET', '')
