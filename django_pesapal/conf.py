b3431fee9d324ccd1b3953cf5caabd4c24f23ca7
Trevor Mutune <metatrevor@gmail.com>
2015-01-05 12:42:45 +0300

    Fix query payment status link

--- django_pesapal/conf.py
+++ django_pesapal/conf.py
@@ -11,7 +11,7 @@ if settings.configured:
         PESAPAL_QUERY_STATUS_LINK = getattr(settings, 'PESAPAL_QUERY_STATUS_LINK', 'http://demo.pesapal.com/API/QueryPaymentStatus')
     else:
         PESAPAL_IFRAME_LINK = getattr(settings, 'PESAPAL_IFRAME_LINK', 'https://www.pesapal.com/api/PostPesapalDirectOrderV4')
-        PESAPAL_QUERY_STATUS_LINK = getattr(settings, 'PESAPAL_QUERY_STATUS_LINK', 'http://demo.pesapal.com/API/QueryPaymentStatus')
+        PESAPAL_QUERY_STATUS_LINK = getattr(settings, 'PESAPAL_QUERY_STATUS_LINK', 'https://www.pesapal.com/API/QueryPaymentStatus')
 
     PESAPAL_CONSUMER_KEY = getattr(settings, 'PESAPAL_CONSUMER_KEY', '')
     PESAPAL_CONSUMER_SECRET = getattr(settings, 'PESAPAL_CONSUMER_SECRET', '')
