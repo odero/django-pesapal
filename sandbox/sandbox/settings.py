7f300049149e972de657220a0be1cb27a6e1033b
Trevor Mutune <metatrevor@gmail.com>
2015-01-05 10:45:17 +0300

    Fix log formatter

--- sandbox/sandbox/settings.py
+++ sandbox/sandbox/settings.py
@@ -100,7 +100,6 @@ LOGGING = {
         'console': {
             'level': 'DEBUG',
             'class': 'logging.StreamHandler',
-            'formatter': 'simple'
         },
     },
     'loggers': {
