04f818580031301456ed13c40de94a46fa59133e
Trevor Mutune <metatrevor@gmail.com>
2015-01-24 23:05:09 +0300

    Refactoring fix

--- django_pesapal/models.py
+++ django_pesapal/models.py
@@ -3,13 +3,13 @@ from django.db import models
 
 class BaseTransaction(models.Model):
     pesapal_transaction_id = models.CharField(max_length=32, null=True, blank=True, unique=True)
-    merchant_reference = models.IntegerField(db_index=True)
+    pesapal_merchant_reference = models.IntegerField(db_index=True)
     amount = models.DecimalField(decimal_places=2, max_digits=10, default=0)
     created = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        unique_together = (('merchant_reference', 'pesapal_transaction'),)
+        unique_together = (('pesapal_transaction_id', 'pesapal_merchant_reference'),)
         abstract = True
 
     def __unicode__(self):
-        return u'Transaction: {0}, Campaign_id: {1}'.format(self.pesapal_transaction, self.merchant_reference)
+        return u'Transaction: {0}, Campaign_id: {1}'.format(self.pesapal_transaction_id, self.pesapal_merchant_reference)
