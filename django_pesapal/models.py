57e624e1a87e87bc69eefccc28ca7ed7cbb50bc0
Trevor Mutune <metatrevor@gmail.com>
2015-01-24 22:45:15 +0300

    Change the pesapal transaction id to a charfield since the uuidfield is an overkill for a bare string that does not need parsing.

--- django_pesapal/models.py
+++ django_pesapal/models.py
@@ -1,11 +1,9 @@
 from django.db import models
 
-from uuidfield import UUIDField
-
 
 class BaseTransaction(models.Model):
-    pesapal_transaction_id = UUIDField(hyphenate=True)
-    pesapal_merchant_reference = models.IntegerField(db_index=True)
+    pesapal_transaction_id = models.CharField(max_length=32, null=True, blank=True, unique=True)
+    merchant_reference = models.IntegerField(db_index=True)
     amount = models.DecimalField(decimal_places=2, max_digits=10, default=0)
     created = models.DateTimeField(auto_now_add=True)
 
