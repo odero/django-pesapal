173c63b4d6bad9f141fe7438aa931027ba036282
Trevor M <metatrevor@gmail.com>
2015-01-28 15:55:51 +0300

    Currency support

--- django_pesapal/models.py
+++ django_pesapal/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
-
 class BaseTransaction(models.Model):
     pesapal_transaction_id = models.CharField(max_length=32, null=True, blank=True, unique=True)
     pesapal_merchant_reference = models.IntegerField(db_index=True)
     amount = models.DecimalField(decimal_places=2, max_digits=10, default=0)
+    currency = models.CharField(max_length=8, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
 
     PENDING, COMPLETED, FAILED, INVALID = 'PENDING', 'COMPLETED', 'FAILED', 'INVALID'
