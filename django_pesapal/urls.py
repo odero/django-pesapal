e64ab3c27b2b73b05bb48d05166386adffb0197c
Trevor M <metatrevor@gmail.com>
2015-01-28 15:56:57 +0300

    Exempt ipn response from csrf

--- django_pesapal/urls.py
+++ django_pesapal/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns, url
+from django.views.decorators.csrf import csrf_exempt
 
 from .views import TransactionCompletedView, IpnResponseHandlerView
 
 urlpatterns = patterns(
     '',
     url(r'^transaction/completed/$', TransactionCompletedView.as_view(), name='transaction_completed'),
-    url(r'^transaction/notification/$', IpnResponseHandlerView.as_view(), name='transaction_notification')
+    url(r'^transaction/notification/$', csrf_exempt(IpnResponseHandlerView.as_view()), name='transaction_notification')
 )
