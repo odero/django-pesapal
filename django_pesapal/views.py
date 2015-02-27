68a7fd8a444a8c568d716db11849f58ad7a9dee5
Trevor Mutune <metatrevor@gmail.com>
2015-02-27 23:42:39 +0300

    Add support for further processing of the payment while maintaining compatibility

--- django_pesapal/views.py
+++ django_pesapal/views.py
@@ -1,9 +1,8 @@
 # Create your views here.
 
-from django.core.urlresolvers import reverse_lazy
-from django.contrib.auth.decorators import login_required
+from django.core.urlresolvers import reverse_lazy, reverse
 from django.views.generic.base import RedirectView
-from django.db.models.loading import get_model
+from django.core.urlresolvers import NoReverseMatch
 
 from .models import Transaction
 
@@ -12,7 +11,7 @@ import conf
 
 class TransactionCompletedView(RedirectView):
     permanent = False
-    url = reverse_lazy(conf.PESAPAL_TRANSACTION_DEFAULT_REDIRECT_URL)
+    url = None
 
     def get(self, request, *args, **kwargs):
         '''
@@ -21,10 +20,20 @@ class TransactionCompletedView(RedirectView):
 
         For further processing just create a `post_save` signal on the `Transaction` model.
         '''
-        transaction_id = request.GET.get('pesapal_transaction_tracking_id', '')
-        merchant_reference = request.GET.get('pesapal_merchant_reference', '')
+        self.transaction_id = request.GET.get('pesapal_transaction_tracking_id', '')
+        self.merchant_reference = request.GET.get('pesapal_merchant_reference', '')
 
-        if transaction_id and merchant_reference:
-            transaction, created = Transaction.objects.get_or_create(merchant_reference=merchant_reference, pesapal_transaction=transaction_id)
+        if self.transaction_id and self.merchant_reference:
+            transaction, created = Transaction.objects.get_or_create(merchant_reference=self.merchant_reference,
+                                                                     pesapal_transaction=self.transaction_id)
 
         return super(TransactionCompletedView, self).get(request, *args, **kwargs)
+
+    def get_redirect_url(self, *args, **kwargs):
+
+        try:
+            url = reverse(conf.PESAPAL_TRANSACTION_DEFAULT_REDIRECT_URL)
+        except NoReverseMatch:
+            url = reverse_lazy(conf.PESAPAL_TRANSACTION_DEFAULT_REDIRECT_URL,
+                           kwargs={'merchant_reference': self.merchant_reference})
+        return url
