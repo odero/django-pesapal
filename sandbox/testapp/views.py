4d2cb2b24f6ec746dcdc4f7a17ed7f528ae066e5
odero <billyx5@gmail.com>
2015-06-13 17:28:21 +0100

    Cleanup testapp

--- sandbox/testapp/views.py
+++ sandbox/testapp/views.py
@@ -1,6 +1,9 @@
+
+from django.conf import settings
+from django.db.models.loading import get_model
 from django.views.generic import TemplateView
+
 from django_pesapal.views import PaymentRequestMixin
-from django_pesapal.models import Transaction
 
 
 class PaymentView(TemplateView, PaymentRequestMixin):
@@ -20,32 +23,4 @@ class PaymentView(TemplateView, PaymentRequestMixin):
         }
 
         ctx['pesapal_url'] = self.get_payment_url(**order_info)
-        return ctx
-
-
-class ResponseView(TemplateView, PaymentRequestMixin):
-    """
-    Payment Response View
-    """
-    template_name = 'testapp/response.html'
-
-    def get_context_data(self, **kwargs):
-        ctx = super(ResponseView, self).get_context_data(**kwargs)
-
-        merchant_reference = self.request.GET.get('merchant_reference', '')
-
-        if merchant_reference:
-            try:
-                txn = Transaction.objects.get(merchant_reference=merchant_reference)
-                params = {
-                    'pesapal_merchant_reference': txn.merchant_reference,
-                    'pesapal_transaction_tracking_id': txn.pesapal_transaction,
-                }
-
-                # In production, the following call should be made asynchronously with something like celery
-                ctx['response'] = self.get_payment_status(**params)
-            except Transaction.DoesNotExist, e:
-                # In production, deal with this dont just print e
-                print e
-
         return ctx
