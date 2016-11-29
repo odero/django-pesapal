d1e4ba618cbbbc28b507d11937f0a2ce1ea0e29e
BitBuckEdd <kamande.edwin@yahoo.com>
2016-11-29 17:03:10 +0300

    removed patterns import from urls.py

--- django_pesapal/urls.py
+++ django_pesapal/urls.py
@@ -2,12 +2,12 @@
 
 from __future__ import absolute_import
 
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 from . import views
 
-urlpatterns = patterns[
-    '',
+urlpatterns = [
+  
     url(r'^transaction/completed/$', views.TransactionCompletedView.as_view(), name='transaction_completed'),
     url(r'^transaction/status/$', views.TransactionStatusView.as_view(), name='transaction_status'),
     url(r'^transaction/ipn/$', views.IPNCallbackView.as_view(), name='transaction_ipn'),
